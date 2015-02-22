f6d266259716b330d45ab19929bf8978123445b7
Alessandro Di Maria <adm@m42.ch>
2015-02-22 22:38:45 +0100

    + count term on linked pages

--- app/services/wiki_fetcher.rb
+++ app/services/wiki_fetcher.rb
@@ -10,7 +10,23 @@ class WikiFetcher
     html = fetch_term(name).body
     File.write(Rails.root.join('tmp', 'example_raw.html'), html.force_encoding("UTF-8"))
     text = filter_text(html)
-    WikiParser.parse(text)
+    WikiParser.parse(name, text)
+  end
+
+  def get_linked_pages linked_pages
+    hydra = Typhoeus::Hydra.new
+    requests = linked_pages.inject({}) do |requests, page|
+      request = term_request(page)
+      hydra.queue(request)
+      requests[page] = request
+      requests
+    end
+    hydra.run
+    reponses = requests.each { |page, request|
+      html = request.response.body
+      markup = filter_text(html)
+      requests[page] = markup
+    }
   end
 
   def term_request name
--- app/services/wiki_parser.rb
+++ app/services/wiki_parser.rb
@@ -2,14 +2,15 @@ class WikiParser < WikiCloth::Parser
 
   include ActionView::Helpers::SanitizeHelper
 
-  attr_reader :html, :text
+  attr_reader :html, :text, :term
 
-  def self.parse(text)
-    new(data: text)
+  def self.parse(name, text)
+    new(data: text, term: name)
   end
 
   def initialize(options)
     super
+    @term = options[:term]
     @html = to_html
     @text = strip_tags(@html)
     File.write(Rails.root.join('tmp', 'example.html'), @html)
@@ -21,14 +22,27 @@ class WikiParser < WikiCloth::Parser
   end
 
   def weight_linked_terms
-    linked_terms.inject({}) do |hash, linked_term|
-      found = html.scan(/\b#{linked_term[1]}\b/).size
-      hash[linked_term[0]] ||= 0
-      hash[linked_term[0]] += found
-      hash
-    end.sort_by do |key, value|
-      value
-    end.reverse
+    count_linked_terms
+    count_on_linked_terms
+  end
+
+  def count_linked_terms
+    count_term(linked_terms) do |item|
+      count_found_term(text, item[1])
+    end
+  end
+
+  def count_on_linked_terms
+    linked_pages = linked_terms.map {|term| term[0] }
+    responses = WikiFetcher.new.get_linked_pages(linked_pages)
+
+    count_term(responses) do |item|
+      count_found_term(item[1], term)
+    end
+  end
+
+  def count_found_term text, term
+    text.scan(/\b#{term}\b/).size
   end
 
   def markup
@@ -41,5 +55,19 @@ class WikiParser < WikiCloth::Parser
     self.internal_links << [page, ltitle]
     elem.a(link_attributes_for(page)) { |x| x << ltitle.strip }
   end
+
+  private
+
+  # list [ [page, name] ]
+  def count_term list
+    list.inject({}) do |hash, term|
+      hash[term[0]] ||= 0
+      hash[term[0]] += yield(term)
+      hash
+    end.sort_by do |key, value|
+      value
+    end.reverse
+  end
+
 
 end
